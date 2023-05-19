4698d30311b501000fda9a2737ea563fea1ca044
Tim Schneider <43130816+DerStimmler@users.noreply.github.com>
2023-05-19 16:29:00 +0200

    refactor: code cleanup

--- StromGedacht.NET/Models/RegionState.cs
+++ StromGedacht.NET/Models/RegionState.cs
@@ -1,27 +1,27 @@
 ﻿namespace StromGedacht.NET.Models;
 
 /// <summary>
-/// Describes different region states
+///   Describes different region states
 /// </summary>
 public enum RegionState
 {
   /// <summary>
-  /// Normal operation - nothing to do
+  ///   Normal operation - nothing to do
   /// </summary>
   Green = 1,
-  
+
   /// <summary>
-  /// Bring consumption forward - use electricity now
+  ///   Bring consumption forward - use electricity now
   /// </summary>
   Yellow = 2,
-  
+
   /// <summary>
-  /// Reduce consumption to save costs and CO2
+  ///   Reduce consumption to save costs and CO2
   /// </summary>
   Orange = 3,
-  
+
   /// <summary>
-  /// Reduce consumption to prevent power shortage
+  ///   Reduce consumption to prevent power shortage
   /// </summary>
   Red = 4
 }
--- StromGedacht.NET/Utils/ApiAddresses.cs
+++ StromGedacht.NET/Utils/ApiAddresses.cs
@@ -6,11 +6,12 @@ internal static class ApiAddresses
 {
   internal static readonly Uri BaseAddress = new("https://api.stromgedacht.de/v1/");
   internal static string Now(string zip) => new($"now?zip={zip}");
+
   internal static string States(string zip, DateTimeOffset from, DateTimeOffset to)
   {
     var encodedFrom = UrlEncoder.Default.Encode(from.ToString("O"));
     var encodedTo = UrlEncoder.Default.Encode(to.ToString("O"));
-    
+
     return $"states?zip={zip}&from={encodedFrom}&to={encodedTo}";
   }
 }
