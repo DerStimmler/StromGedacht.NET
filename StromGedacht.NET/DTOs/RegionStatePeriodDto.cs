fdf79e632335675553b3a6e3f8d05e82c454aec1
Tim Schneider <43130816+DerStimmler@users.noreply.github.com>
2024-01-17 19:13:36 +0100

    refactor: use dto for communication with api

---
+++ StromGedacht.NET/DTOs/RegionStatePeriodDto.cs
@@ -0,0 +1,10 @@
+﻿using StromGedacht.NET.Models;
+
+namespace StromGedacht.NET.DTOs;
+
+internal record RegionStatePeriodDto
+{
+  public RegionState State { get; set; }
+  public DateTimeOffset From { get; set; }
+  public DateTimeOffset To { get; set; }
+}
